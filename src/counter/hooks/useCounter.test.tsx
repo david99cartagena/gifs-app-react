6325c15a6590586bfaafa1f2f757053c14cee2ef
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-03-21 23:11:00 -0500

    test de componente de complemento

---
+++ src/counter/hooks/useCounter.test.tsx
@@ -0,0 +1,65 @@
+import { describe, expect, test } from "vitest";
+import { useCounter } from "./useCounter";
+import { act, renderHook } from "@testing-library/react";
+
+describe("useCounter", () => {
+  test("should initialize with default value of 10", () => {
+    const { result } = renderHook(() => useCounter());
+
+    expect(result.current.counter).toBe(10);
+  });
+
+  test("should initialize with value 20", () => {
+    const initialValue = 20;
+
+    const { result } = renderHook(() => useCounter(initialValue));
+
+    expect(result.current.counter).toBe(initialValue);
+  });
+
+  test("should increment counter when handleAdd is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    // el act de testing library
+    act(() => {
+      result.current.handleAdd();
+    });
+    
+    /* act(() => {
+      result.current.handleAdd();
+    }); 12 */
+
+    expect(result.current.counter).toBe(11);
+  });
+
+  test("should decrement counter when handleSubtract is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleSubtract();
+    });
+
+    expect(result.current.counter).toBe(9);
+  });
+
+  test("should reset to initialValue the counter when handleReset is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleSubtract();
+      result.current.handleSubtract();
+      result.current.handleSubtract();
+      result.current.handleSubtract();
+      result.current.handleSubtract();
+      // result.current.handleReset(); no muy recomendado
+    });
+
+    expect(result.current.counter).toBe(5);
+
+    act(() => {
+      result.current.handleReset();
+    });
+
+    expect(result.current.counter).toBe(10);
+  });
+});
